c6f33ad9a0f0a96ff92b325614968d20ec027fb3
Luis Bazán <luis@bazan.pe>
2022-11-12 10:58:46 -0500

    Player's heads drops temporaly

--- src/main/java/pe/bazan/luis/plugins/coldfeeling/events/PlayerDetect.java
+++ src/main/java/pe/bazan/luis/plugins/coldfeeling/events/PlayerDetect.java
@@ -1,14 +1,25 @@
 package pe.bazan.luis.plugins.coldfeeling.events;
 
+import dev.dbassett.skullcreator.SkullCreator;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentBuilder;
+import net.kyori.adventure.text.TextComponent;
+import net.skinsrestorer.api.SkinsRestorerAPI;
+import org.bukkit.ChatColor;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import pe.bazan.luis.plugins.coldfeeling.ColdFeeling;
 import pe.bazan.luis.plugins.coldfeeling.instances.ColdPlayer;
 
 public class PlayerDetect implements Listener {
   private ColdFeeling coldFeeling = ColdFeeling.getInstance();
+  private SkinsRestorerAPI restorerAPI = SkinsRestorerAPI.getApi();
 
   @EventHandler
   public void onPlayerJoin(PlayerJoinEvent e) {
@@ -19,5 +30,14 @@ public class PlayerDetect implements Listener {
   public void onPlayerQuit(PlayerQuitEvent e) {
     if(coldFeeling.getColdPlayers().containsKey(e.getPlayer().getName()))
       coldFeeling.getColdPlayers().remove(e.getPlayer().getName());
+  }
+
+  @EventHandler
+  public void onPlayerDeath(PlayerDeathEvent e) {
+    ItemStack head = SkullCreator.itemFromBase64(restorerAPI.getSkinData(restorerAPI.getSkinName(e.getPlayer().getName())).getValue());
+    ItemMeta meta = head.getItemMeta();
+    meta.setDisplayName(ChatColor.RESET + e.getPlayer().getName()+"'s Head");
+    head.setItemMeta(meta);
+    e.getPlayer().getWorld().dropItemNaturally(e.getPlayer().getLocation(), head);
   }
 }
